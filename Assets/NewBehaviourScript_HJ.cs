e7fe9715b8cc4d0483ee80b27dc648f1e19e6790
mhj0001 <moonhojo@naver.com>
2023-03-11 20:09:35 +0900

    HJ 23.03.11
    HJ 23.03.11

---
+++ Assets/NewBehaviourScript_HJ.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript_HJ : MonoBehaviour
+{
+    int point_HJ = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void addPoint(){
+        point_HJ++;
+    }
+
+    void initPoint(){
+        point_HJ = 0;
+    }
+}
